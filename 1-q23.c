e6d6c0f57fcd70397d87e49e73e5e6ab0a782130
ashika29 <49125522+ashika29@users.noreply.github.com>
2019-04-16 17:36:13 +0530

    Add files via upload

---
+++ 1-q23.c
@@ -0,0 +1,9 @@
+// A Multithreaded Program that implements the banker's algorithm and prints safe sequence if any:
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <time.h>
+
